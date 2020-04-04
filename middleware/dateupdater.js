9666e903b65b1ff092537908d5878430ccb57633
THIMM Marc-Antoine <47699758+marcantoinethimm@users.noreply.github.com>
2020-04-04 02:10:34 -0500

    Date comparaison error fixed

--- middleware/dateupdater.js
+++ middleware/dateupdater.js
@@ -2,16 +2,13 @@ const User = require('../models/User.js')
 
 module.exports = (req, res, next) => {
 
-  var dateObj = new Date();
-  var month = dateObj.getUTCMonth() + 1; //months from 1-12
-  var day = dateObj.getUTCDate();
-  var year = dateObj.getUTCFullYear();
-
-  dateNow = Date(year + "-" + month + "-" + day);
+  var dateNow = new Date();
+  dateNow.setHours(0,0,0,0);
 
   User.findOne({_id: req.body.userId})
     .then(user =>{
-      console.log(user.date);
+      //fix the time to 00h, 0 min , 0 sec
+      user.date.setHours(0,0,0,0);
       if (user.date < dateNow){
         User.updateOne({ _id: req.body.userId }, { words_count: 0 }, { date: dateNow})
         .catch(error => res.status(400).json({ error }));
